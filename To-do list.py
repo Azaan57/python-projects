829c10c680c8e340745d3e2923c8e8e701da13dc
Azaan Azam <whamshell2@gmail.com>
2026-06-14 18:32:17 +0100

    added main class to wrap around program, created extra objects+classes for functions

--- To-do list.py
+++ To-do list.py
@@ -8,38 +8,47 @@ def remove_item():
             else:
                 print("Try again")
 
-print("Welcome to your to do list")
+def add_item():
+    n = int(input("Please enter the number of items you want to add to the list: "))
 
-while True:
+    for i in range(n):
+        item = str(input("Now please enter the item you wish to add to the list: ")).lower()
+        to_do_list.append(item)
+    print("Items added.")
 
-    choice = input(
-        "\nWhat would you like to do?\n" 
-        "1) Add\n" 
-        "2) Remove\n"
-        "3) View list\n" 
-        "4) Quit\n"
-        "Enter your choice: "
-    )
-    if choice == "1" or "Add".lower():
+def list_choice():
+     return input(
+            "\nWhat would you like to do?\n" 
+            "1) Add\n" 
+            "2) Remove\n"
+            "3) View list\n" 
+            "4) Quit\n"
+            "Enter your choice: "
+        ).strip().lower()
 
-        n = int(input("Please enter the number of items you want to add to the list: "))
+def main():
+    print("Welcome to your to do list")
 
-        for i in range(n):
-            item = str(input("Now please enter the item you wish to add to the list: ")).lower()
-            to_do_list.append(item)
-        print("Items added.")
+    while True:
 
-    elif choice == "2" or "Remove".lower():
-        remove_item()
+        choice = list_choice()
         
-    elif choice == "3" or "View list".lower():
-         print(to_do_list)
+        if choice in {"1", "add"}:
+            add_item()
 
-    elif choice == "4" or "Quit".lower():
-         break
+        elif choice in {"2", "remove"}:
+            remove_item()
+        
+        elif choice in {"3", "view", "view list"}:
+            print(to_do_list)
+
+        elif choice in {"4", "quit"}:
+            break
     
     
-    again = input("Enter to restart or type quit to exit")
-    if again == "quit":
-         
-        break
+        again = input("Enter to restart or type quit to exit. ")
+        if again == "quit":
+            break
+
+if __name__ == "__main__":
+     main()
